c3fbb11ec74d53246faf479c6e1b023f29500c34
Alejo Next <alejonext>
2016-10-28 14:35:45 -0500

    Card - La carta de creditCard

--- src/screens/Calculations.js
+++ src/screens/Calculations.js
@@ -3,15 +3,59 @@ import {
   StyleSheet,
   View,
   Text,
+
 } from 'react-native';
+import db from '../models';
+import CreditCard from '../components/card.js';
+
 
 export default class CalculationsScreen extends Component {
-  render() {
+
+  renderList(){
     return (
-      <View style={styles.card}>
-        <Text>Calculations screen</Text>
+    <View>
+      <View>
+        <Text>Valor</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
       </View>
-    );  
+      <View>
+        <Text>Coutas</Text>
+        <TextInput
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+      </View>
+
+
+      <ListView
+        dataSource={this.state.listCards}
+        renderRow={data => this.renderCard(data)} />
+      <Button
+        icon={{name: 'cached'}}
+        title='Agregar '
+        onPress={()=> this.setState({ openNew : true }) }/>
+    </View>);
+  }
+
+
+  renderRow(data){
+    return (
+      <CreditCard typeCard="visa" name="holi122">
+        <Text>Blas</Text>
+      </CreditCard> );
+
+  }
+
+  render(){
+    return
+    <CreditCard typeCard="visa" name="holi122">
+      <Text>Blas</Text>
+    </CreditCard>
   }
 }
 
