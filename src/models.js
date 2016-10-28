73700770676ca1a35c67fa06d79f6845e4997f8c
khriztianmoreno <khriztianmoreno@gmail.com>
2016-10-28 15:48:00 -0500

    Create realm wrapper

--- src/models.js
+++ src/models.js
@@ -2,8 +2,9 @@ import Realm from 'realm';
 
 const cardCreditSchema = {
 	name  : 'Card',
-	schemaVersion : 1,
+	primaryKey: 'id',
 	properties : {
+		id	 : 'int',
 		name : { type : 'string', indexed : true },
 		tax  : { type : 'int', default: 0},
 		cupo : { type : 'int', default: 0}, // Asi?
@@ -13,12 +14,46 @@ const cardCreditSchema = {
 
 const userPhoneSchema = {
 	name  : 'userPhone',
-	schemaVersion : 1,
+	primaryKey: 'id',
 	properties : {
+		id	  : 'int',
 		name  : 'string',
 		email : { type: 'string', optional: true},
 		token : 'string'
 	}
 };
 
-export default new Realm({schema: [userPhoneSchema, cardCreditSchema]});
+const realm = new Realm({schema: [userPhoneSchema, cardCreditSchema]});
+
+const DB = {
+	add: (model, data) => {
+		realm.write(() => {
+		  realm.create(model, data);
+		});
+	},
+	update: (model, data) => {
+		realm.write(() => {
+		  realm.create(model, data, true);
+		});
+	},
+	clean: (model) => {
+		realm.write(() => {
+			let _modelDelete = realm.objects(model);
+  		realm.delete(_modelDelete);
+		})
+	},
+	list: (model, sort) => {
+		if (sort)
+			return realm.objects(model).sorted(sort);
+		else
+			return realm.objects(model);
+	},
+	search: (model, query, sort) => {
+		if (sort)
+			return realm.objects(model).filtered(query).sorted(sort);
+		else
+			return realm.objects(model).filtered(query).sorted(sort);
+	}
+};
+
+export default DB;
